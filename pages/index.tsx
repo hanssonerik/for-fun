78b5c31072ec5fe331c12edba7de0f39c6a9da87
Erik Hansson <hansson_erik@outlook.com>
2021-07-08 23:27:10 +0200

    feature: add page and keep it simple

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,26 @@
-export { Home as default } from 'components/Home'
+import React, { ReactElement } from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+
+const Home = (): ReactElement => {
+  return (
+    <>
+      <Head>
+        <title>This is just for fun</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <h1>Learning new stuff is fun...</h1>
+        <p>
+          Next thing I want to add is some styling, and maybe a new page with
+          some other fun stuff. I&apos;ll add a{' '}
+          <Link href="/random-picture">link</Link>, just in case.
+        </p>
+      </div>
+    </>
+  )
+}
+
+Home.whyDidYouRender = true
+
+export default Home
---
+++ pages/random-picture.tsx
@@ -0,0 +1,23 @@
+import React, { ReactElement } from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+
+const RandomPicture = (): ReactElement => {
+  return (
+    <>
+      <Head>
+        <title>A random picture - just for fun</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <h1>Random picture please!</h1>
+        <p>
+          {`If you don't like it you can always go`} <Link href="/">home</Link>,
+          just in case.
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default RandomPicture
