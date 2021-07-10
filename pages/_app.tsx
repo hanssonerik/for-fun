f23dfb826fecd5759fa3bfe631abf7c70559bd98
Erik Hansson <hansson_erik@outlook.com>
2021-07-11 00:18:26 +0200

    feat: move Theme Provider to _app

---
+++ pages/_app.tsx
@@ -0,0 +1,62 @@
+import 'scripts/wdyr'
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { pageview } from '../lib/gtag'
+import {
+  DefaultTheme,
+  ThemeProvider,
+  createGlobalStyle
+} from 'styled-components'
+import { AppProps } from 'next/dist/next-server/lib/router/router'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  const [theme] = useState(lightTheme)
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageview(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <>
+      <Global />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
+
+const lightTheme: DefaultTheme = {
+  palette: {
+    background: {
+      main: '#2F2F2F',
+      contrastText: '#000000',
+      variant: '#F6F6F6'
+    },
+    error: { main: '#C63D3D', contrastText: '#FFFFFF', variant: '#D77575' },
+    surface: {
+      main: '#2F2F2F',
+      contrastText: '#000000',
+      variant: '#F6F6F6'
+    },
+    primary: { main: '#2BB3CF', contrastText: '#FFFFFF', variant: '#124D59' },
+    secondary: { main: '#4B2BCF', contrastText: '#FFFFFF', variant: '#361E94' }
+  }
+}
+
+const Global = createGlobalStyle`
+   body { padding: 0;
+    margin: 0;
+    }
+`
+MyApp.whyDidYouRender = true
+
+export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,51 +1,37 @@
 import React, { ReactElement, useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
-import styled, {
-  DefaultTheme,
-  ThemeProvider,
-  createGlobalStyle
-} from 'styled-components'
+import styled from 'styled-components'
 
 const Home = (): ReactElement => {
-  const [theme, setTheme] = useState(lightTheme)
   return (
-    <ThemeProvider theme={theme}>
-      <Global />
-      <Background>
-        <Head>
-          <title>This is just for fun</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Wrapper>
-          <Box>
-            <Title>Learning new stuff is fun...</Title>
-            <p>
-              Next thing I want to add is some styling, and maybe a new page
-              with some other fun stuff. I&apos;ll add a{' '}
-              <StyledLink href="/random-picture">link</StyledLink>, just in
-              case.
-            </p>
-          </Box>
-          <BoxVariant>
-            <Title>Learning about old stuff is also fun...</Title>
-            <p>I hope this gets good case.</p>
-          </BoxVariant>
-          <ErrorBox>
-            <Title>Warning, boring is not so fun</Title>
-            <p>I hope this gets good case.</p>
-          </ErrorBox>
-        </Wrapper>
-      </Background>
-    </ThemeProvider>
+    <Background>
+      <Head>
+        <title>This is just for fun</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Wrapper>
+        <Box>
+          <Title>Learning new stuff is fun...</Title>
+          <p>
+            Next thing I want to add is some styling, and maybe a new page with
+            some other fun stuff. I&apos;ll add a{' '}
+            <StyledLink href="/random-picture">link</StyledLink>, just in case.
+          </p>
+        </Box>
+        <BoxVariant>
+          <Title>Learning about old stuff is also fun...</Title>
+          <p>I hope this gets good case.</p>
+        </BoxVariant>
+        <ErrorBox>
+          <Title>Warning, boring is not so fun</Title>
+          <p>I hope this gets good case.</p>
+        </ErrorBox>
+      </Wrapper>
+    </Background>
   )
 }
 
-const Global = createGlobalStyle`
-   body { padding: 0;
-    margin: 0;
-    }
-`
 const Background = styled.div`
   background: ${(props) => props.theme.palette.background.main};
   padding: 1em;
@@ -75,24 +61,6 @@ const StyledLink = styled(Link)`
   color: ${(props) => props.theme.palette.error.contrastText};
   background: ${(props) => props.theme.palette.secondary.contrastText};
 `
-
-const lightTheme: DefaultTheme = {
-  palette: {
-    background: {
-      main: '#2F2F2F',
-      contrastText: '#000000',
-      variant: '#F6F6F6'
-    },
-    error: { main: '#C63D3D', contrastText: '#FFFFFF', variant: '#D77575' },
-    surface: {
-      main: '#2F2F2F',
-      contrastText: '#000000',
-      variant: '#F6F6F6'
-    },
-    primary: { main: '#2BB3CF', contrastText: '#FFFFFF', variant: '#124D59' },
-    secondary: { main: '#4B2BCF', contrastText: '#FFFFFF', variant: '#361E94' }
-  }
-}
 
 const Title = styled.h1`
   font-size: 1.5em;
