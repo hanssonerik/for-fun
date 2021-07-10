5d8017c5f5a516560395c77f917e94d4574bf229
Erik Hansson <hansson_erik@outlook.com>
2021-07-11 00:25:17 +0200

    chore: remove rule for React in scope
    Not needed for React 17+

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,8 @@ module.exports = {
   ],
   rules: {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-unused-vars': [
